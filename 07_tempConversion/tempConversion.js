e0c9f478b8b358ba6e559587bc936d0079941597
TomStlau <tommy.stlau@gmail.com>
2023-10-27 18:39:20 -0400

    Completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (temp) {
+  celsiusTemp = +((temp - 32) / 1.8).toFixed(1)
+  return celsiusTemp
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function (temp) {
+  fahrenheitTemp = +(temp * 1.8 + 32).toFixed(1)
+  return fahrenheitTemp
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
-};
+}

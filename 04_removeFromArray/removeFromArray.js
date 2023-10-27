dc5f2caad7f3b08422c9a94cfaad81e4d639f18e
TomStlau <tommy.stlau@gmail.com>
2023-10-27 17:47:02 -0400

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (list, ...items) {
+  for (let i = 0; i < items.length; i++) {
+    if (list.includes(items[i])) {
+      let itemId = list.indexOf(items[i])
+      list.splice(itemId, 1)
+    }
+  }
+  return list
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray

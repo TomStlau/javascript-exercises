dab735771dc2278782653efff6c9c3ea783b49f9
TomStlau <tommy.stlau@gmail.com>
2023-10-27 18:18:37 -0400

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
-};
+const sumAll = function (firstNum, lastNum) {
+  if (
+    firstNum < 0 ||
+    lastNum < 0 ||
+    typeof firstNum != 'number' ||
+    typeof lastNum != 'number'
+  )
+    return 'ERROR'
+  if (lastNum > firstNum) {
+    let nextNum = firstNum + 1
+    for (let i = firstNum; i < lastNum; i++) {
+      firstNum += nextNum
+      nextNum++
+    }
+    return firstNum
+  } else {
+    let nextNum = firstNum - 1
+    for (let i = firstNum; i > lastNum; i--) {
+      firstNum += nextNum
+      nextNum--
+    }
+    return firstNum
+  }
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll

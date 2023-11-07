b3c3dff240a59717f40d58f811fd040c09810d2d
TomStlau <tommy.stlau@gmail.com>
2023-11-06 21:07:47 -0500

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (number < 0) return 'OOPS'
+  let n1 = 0,
+    n2 = 1,
+    next
 
-};
+  for (i = 0; i < number; i++) {
+    console.log(n1)
+    next = n1 + n2
+    n1 = n2
+    n2 = next
+  }
+  return n1
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci

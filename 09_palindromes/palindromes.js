8bde8d3140807ee0bfe23464ca5311e8e37cd3f9
TomStlau <tommy.stlau@gmail.com>
2023-11-06 20:55:40 -0500

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+  const strippedWord = word
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+    .replaceAll(' ', '')
+    .toLowerCase()
+  const reversedWord = strippedWord.split('').reverse().join('')
+  if (strippedWord === reversedWord) return true
+  return false
+}
+console.log(palindromes('Animal loots foliated detail of stool lamina.'))
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes

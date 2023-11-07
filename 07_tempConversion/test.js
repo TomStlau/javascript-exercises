d017488305de53aff7b651c6e05212e1e794110b
TomStlau <tommy.stlau@gmail.com>
2023-11-06 20:08:14 -0500

    Completed 08

---
+++ 07_tempConversion/test.js
@@ -0,0 +1,5 @@
+const array1 = [1, 2, 3, 4, 5, 6]
+const array2 = [...array1]
+
+console.log(array2)
+
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
-};
+const add = function (a, b) {
+  return a + b
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (a, b) {
+  return a - b
+}
 
-const sum = function() {
-	
-};
+const sum = function (array) {
+  return array.reduce((number, reducer) => {
+    return (number += reducer)
+  }, 0)
+}
 
-const multiply = function() {
+const multiply = function ([...args]) {
+  return args.reduce((number, reducer) => {
+    return (number *= reducer)
+  }, 1)
+}
 
-};
+const power = function (power, num) {
+  return (power **= num)
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+function factorial (num) {
+  if (typeof num !== 'number' || num < 0 || Math.floor(num) !== num) {
+    throw new Error('Input must be a non-negative integer')
+  }
+  if (num === 1 || num === 0) return 1
+  let result = 1
+  for (let i = 2; i <= num; i++) {
+    result *= i
+  }
+  return result
+}
 
 // Do not edit below this line
 module.exports = {
@@ -30,4 +42,4 @@ module.exports = {
   multiply,
   power,
   factorial
-};
+}
